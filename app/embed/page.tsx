95d8a9af58561008c78d165924f4d18e73093304
lefteriseleftherioule-stack <lefteris.eleftheriou.le@gmail.com>
2025-12-09 08:48:45 +0200

    Update page.tsx

--- app/embed/page.tsx
+++ app/embed/page.tsx
@@ -1,16 +1,12 @@
-import NewsList from "@/components/NewsList"
 import Scoreboard from "@/components/Scoreboard"
-import StandingsTable from "@/components/StandingsTable"
 
 export const dynamic = "force-static"
 
 export default function EmbedPage() {
   return (
     <main className="container">
-      <div className="grid">
-        <NewsList />
+      <div>
         <Scoreboard />
-        <StandingsTable />
       </div>
     </main>
   )
